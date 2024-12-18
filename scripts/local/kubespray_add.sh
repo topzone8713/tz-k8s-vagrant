99256837c2917f8ffd1d0b2e5f279a1dc84f1e6a
doohee323 <doohee323@gmail.com>
2024-12-18 13:44:39 -0800

    containerd/config.toml

--- scripts/local/kubespray_add.sh
+++ scripts/local/kubespray_add.sh
@@ -15,6 +15,10 @@ ansible-playbook -u root -i /vagrant/resource/kubespray/inventory_add.ini \
   --private-key .ssh/tz_rsa --become --become-user=root \
   /vagrant/kubespray/cluster.yml
 
+cp -Rf resource/kubespray/containerd.yml /Volumes/workspace/tz/tz-k8s-vagrant/kubespray/playbooks
+ansible-playbook -u root -i resource/kubespray/inventory_add.ini kubespray/containerd.yml \
+  --become --become-user=root
+
 bash /vagrant/scripts/k8s_addtion.sh
 
 exit 0
